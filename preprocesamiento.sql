33b54d9f120f1e929abef8930083c5389e09b9a4
GabrielaCuervoR <142761963+GabrielaCuervoR@users.noreply.github.com>
2023-08-29 23:13:44 -0500

    Creación base de datos

---
+++ preprocesamiento.sql
@@ -0,0 +1,16 @@
+---Crear base de datos de retirement_info solo con attrition
+
+drop table if exists retirement_new;
+create table retirement_new as
+Select employeeid, attrition from retirement_info;
+
+
+--- Crear la base nueva con las 4 bases de datos juntas
+drop table if exists base_nueva;
+create table base_nueva AS
+SELECT *
+FROM general_data AS gd
+JOIN manager_survey_data AS mgr ON gd.employeeid = mgr.employeeid
+JOIN employee_survey_data AS emp ON gd.employeeid = emp.employeeid
+JOIN retirement_new AS ret ON gd.employeeid = ret.employeeid;
+
