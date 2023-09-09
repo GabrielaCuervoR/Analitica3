cbf418df50e4f4f9415265bd1a99837b01964d6a
GabrielaCuervoR <142761963+GabrielaCuervoR@users.noreply.github.com>
2023-09-09 11:48:11 -0500

    Update preprocesamiento.sql

--- preprocesamiento.sql
+++ preprocesamiento.sql
@@ -10,7 +10,7 @@ drop table if exists base_nueva;
 create table base_nueva AS
 SELECT *
 FROM general_data AS gd
-JOIN manager_survey_data AS mgr ON gd.employeeid = mgr.employeeid
-JOIN employee_survey_data AS emp ON gd.employeeid = emp.employeeid
-JOIN retirement_new AS ret ON gd.employeeid = ret.employeeid;
+LEFT JOIN manager_survey_data AS mgr ON gd.employeeid = mgr.employeeid
+LEFT JOIN employee_survey_data AS emp ON gd.employeeid = emp.employeeid
+LEFT JOIN retirement_new AS ret ON gd.employeeid = ret.employeeid;
 
