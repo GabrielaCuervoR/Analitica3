f0433a1d8634bb57e1a6e3a646a732ba65abbef8
pablitojp1 <115129231+pablitojp1@users.noreply.github.com>
2023-09-11 09:57:10 -0500

    d_modelos.py
    modificaciones

--- d_modelos.py
+++ d_modelos.py
@@ -24,8 +24,69 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import openpyxl
 %matplotlib inline
 
-#### Variables dummis
+# ==============================================================================
+####### 1. Se llama a la base de datos a trabajar
+####### 2. Se verifican datos nulos y faltantes
+####### 3. Se imputan nulos y se eliminan variables de una sola categoria
+####### 4. Se transforma la variable objetivo a numérica
+####### 5. Se dejan todas las variables numéricas en formato int
+####### 6. Variables dummis
+####### 7. Creación de modelos
+####### 8. Selección de variables
+####### 9. Afinamiento de hiperparámetros mejor modelo
+####### 10. Creación de modelo afinado
+####### 11. Entramiento del modelo
+####### 12. Métricas de desempeño
+# ==============================================================================
+#### Se trae la base de datos nueva para iniciar con el modelo ####
 
+conn= sql.connect("db_empleados") ## Con este comando se crea la base de datos o si ya existe se conecta
+df2=pd.read_sql("select * from base_nueva", conn)
+
+### verificación de nulos y datos faltantes
+# ==============================================================================
+df2.isnull().sum() #Mirar la cantidad de nulos de cada variable
+
+### imputación y eliminación de varibles 
+# ==============================================================================
+list_cat=['EnvironmentSatisfaction','JobSatisfaction','WorkLifeBalance']
+list_num=['NumCompaniesWorked','TotalWorkingYears']
+
+funciones.imputar_fc(df2,list_cat)
+funciones.imputar_fn(df2,list_num)
+df2 = df2.drop(['Over18','EmployeeID','EmployeeCount'], axis = 1) #eliminación de variables de una sola categoria
+
+### verificación de la base datos
+# ==============================================================================
+df2.isnull().sum() #se verifica que quede completa
+
+### Transformación de variable objetivo
+# ==============================================================================
+df2['Attrition'] = df2['Attrition'].fillna('No') #se rellenan los nulos de la variable objetivo por No
+df2['Attrition'].value_counts()
+
+#### preparación de los datos 
+# ==============================================================================
+y = df2.Attrition ##Variable Objetivo
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y[0:5])
+
+print(le.classes_)
+
+df2['Attrition'] = y
+
+#### Cambiar tipo de datos de float a int
+# ==============================================================================
+df2.EnvironmentSatisfaction = df2.EnvironmentSatisfaction.astype(int)
+df2.JobSatisfaction  = df2.JobSatisfaction.astype(int)
+df2.WorkLifeBalance = df2.WorkLifeBalance.astype(int)
+df2.NumCompaniesWorked = df2.NumCompaniesWorked.astype(int)
+df2.TotalWorkingYears = df2.TotalWorkingYears.astype(int)
+
+#### Variables dummis
+# ==============================================================================
 df3=df2.copy()
 list_dummies=['BusinessTravel','Department','EducationField','Gender','JobRole','MaritalStatus']
 df_dummies=pd.get_dummies(df3,columns=list_dummies)
@@ -38,18 +99,18 @@ X2=scaler.transform(X1)
 X=pd.DataFrame(X2,columns=X1.columns)
 
 ##### creación de modelos 
-
-m_log = LogisticRegression(max_iter=1000,random_state=42)
-m_rf= RandomForestClassifier(random_state=42)
-m_tc = tree.DecisionTreeClassifier(random_state=42)
-m_gbt=GradientBoostingClassifier(random_state=42)
+# ==============================================================================
+m_log = LogisticRegression(max_iter=1000,random_state=42, class_weight='balanced')
+m_rf= RandomForestClassifier(random_state=42, class_weight='balanced')
+m_tc = tree.DecisionTreeClassifier(random_state=42, class_weight='balanced')
+m_gbt=GradientBoostingClassifier(random_state=42, class_weight='balanced')
 
 
 modelos=list([m_log, m_rf, m_tc, m_gbt])
 
 ### selección de variables 
-
-var_names= funciones.sel_variables(modelos,X,y,threshold="2.35*mean")
+# ==============================================================================
+var_names= funciones.sel_variables(modelos,X,y,threshold="2.35*mean") #se decide usar el 2.35 mean para tener solo 10 variables en el modelo
 var_names.shape
 
 modelo=modelos[0]
@@ -70,7 +131,7 @@ acc_varsel.plot(kind='box') ### gráfico para modelo variables seleccionadas
 acc.plot(kind='box') ### gráfico para modelos sel y todas las variables
 
 ##### afinamiento de hiperparametros 
-
+# ==============================================================================
 # Separación en conjuntos de entrenamiento y validación con 80% de muestras para entrenamiento
 X_train, X_test, y_train, y_test = train_test_split(X2, y, test_size=0.2, random_state=42)
 
@@ -80,6 +141,7 @@ parameters = {'n_estimators': [20,40,80,100,150],
               'max_leaf_nodes': [50,70,100, 300]}
 
 #Definición del modelo
+# ==============================================================================
 rfc = RandomForestClassifier(random_state=42, class_weight='balanced')
 
 grid_search = GridSearchCV(rfc, parameters, cv=5, scoring='f1', n_jobs=-1)
@@ -92,7 +154,7 @@ bestModel_rfc=grid_result.best_estimator_
 print("F1:{:.2f}".format(bestModel_rfc.score(X_test,y_test)))
 
 # ==============================================================================
-#### Entrenamiento de los modelos
+#### Entrenamiento del modelo
 
 ranfor = RandomForestClassifier(
             n_estimators = 150,
