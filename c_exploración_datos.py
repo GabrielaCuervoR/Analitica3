b0b29fc2d3f4cd3eda11c9c463674dab88957b33
GabrielaCuervoR <142761963+GabrielaCuervoR@users.noreply.github.com>
2023-09-11 18:18:23 -0500

    Update c_exploración_datos.py

--- c_exploración_datos.py
+++ c_exploración_datos.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd ### para manejo de datos
 import sqlite3 as sql
 
+cur= conn.cursor()
+funciones.ejecutar_sql('preprocesamiento.sql', cur)
+
+df2=pd.read_sql("select * from base_nueva", conn)
+
+df2 = df2.drop(columns=['index'], axis=1)##Se elimina la columna index que por defecto la sube sql
 
 ##Gráficos variables categóricas
 plt.figure(figsize=(20, 10))
@@ -28,3 +34,26 @@ df2.hist(bins=30, figsize=(20, 15))
 df2.info() #Visualizar tipos de datos de cada variable
 
 df2.describe()
+
+##Explorar variable objetivo
+df2['Attrition'] = df2['Attrition'].fillna('No') #se rellenan los nulos de la variable objetivo por No
+
+#Histograma de la variable objetivo
+df2['Attrition'].value_counts().plot(kind='bar')
+
+y = df2.Attrition ##Variable Objetivo
+# LabelEncoder: Transformar la variable objetivo en numérica.
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y[0:5])
+
+print(le.classes_)
+
+df2['Attrition'] = y
+
+#se hizo la correlación de la variable ojetivo con las numéricas
+corr = df2.corr()
+corr[['Attrition']].sort_values(by = 'Attrition',ascending = False)\
+.style.background_gradient()
